3b4462c5cc26fb7043b9fa2e1b210c524c73dfec
neisinger <Joao.Neisinger@googlemail.com>
2022-11-28 13:12:48 +0100

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -1,6 +1,6 @@
-MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
-RGBLIGHT_ENABLE = no
-TAP_DANCE_ENABLE = yes
-UNICODE_ENABLE = yes
-BACKLIGHT_ENABLE = no
-BOOTMAGIC_ENABLE = yes
+MOUSEKEY_ENABLE = no        # Enable Mouse keys(+4700)
+RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
+TAP_DANCE_ENABLE = yes      # Enable Tap Dance
+UNICODE_ENABLE = yes        # Enable Unicode-Support
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
